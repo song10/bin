2e7777a43fff12b65cad2a4cc240d6e22da5bdd8
song10 <song10@gmail.com>
2012-06-08 19:26:17 +0800

    tweak fuse.js

--- fuse.js
+++ fuse.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
+//(function () {
 var nopt = require('nopt')
 var fs = require('fs')
 var dot = require('dot')
 var path = require('path')
 
+var print = console.log
+
 try { var d4 = require('d4.json') } catch(e) {}
 
-var print = console.log
 var merge = function (obj1, obj2) {
 	var obj3 = {}
 	for (var attrname in obj1) { obj3[attrname] = obj1[attrname] }
@@ -17,7 +19,8 @@ var merge = function (obj1, obj2) {
 var knownOpts = {
 	"template" : [String, null],
 	"init" : Boolean,
-	"force": Boolean
+	"force": Boolean,
+	"strip": Boolean
 	}
 var opt = nopt(knownOpts)
 //console.log(opt)
@@ -46,14 +49,15 @@ if (0 == opt.argv.remain.length) {
 
 var temp = fs.readFileSync(opt.template,'utf-8')
 
-dot.templateSettings.strip = false
+dot.templateSettings.strip = opt.strip
 var fn = dot.template(temp)
 
 opt.argv.remain.forEach(function(x) {
 	var json = require(x)
 	json = merge(opt, json)
-	json.json.path = __dirname + '/../wrk'
+	json._ = {"path":path}
 	var render = fn(json)
 
 	print(render)
 })
+//}).call(this)
