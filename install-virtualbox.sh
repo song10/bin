8687fc3373a07b2fc6a4a3f89fa6792d4052c9f0
song10 <song10@gmail.com>
2014-03-01 07:46:53 +0800

    + install-virtualbox.sh

--- install-virtualbox.sh
+++ install-virtualbox.sh
@@ -4,3 +4,4 @@ echo 'deb http://download.virtualbox.org/virtualbox/debian saucy contrib' | sudo
 wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install -y virtualbox-4.3 dkms
+sudo adduser song10 vboxusers
