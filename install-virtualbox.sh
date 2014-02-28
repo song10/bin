c0dd413d1a5ec64fc8d30098a1177821645dfed5
song10 <song10@gmail.com>
2014-02-28 17:24:08 +0800

    + install-virtualbox.sh

--- install-virtualbox.sh
+++ install-virtualbox.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+sudo sed -i '/deb http:\/\/download.virtualbox.org\/virtualbox\/debian saucy contrib/d' /etc/apt/sources.list
+echo 'deb http://download.virtualbox.org/virtualbox/debian saucy contrib' | sudo tee -a /etc/apt/sources.list
 wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install -y virtualbox-4.3 dkms
