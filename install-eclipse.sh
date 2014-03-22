ee74aedbebeb59fc05a13db3d725b68251d96a0f
song10 <song10@gmail.com>
2014-03-23 04:59:00 +0800

    + install-eclipse.sh

--- install-eclipse.sh
+++ install-eclipse.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
+
+# install pre-required packages
+PREPKG=
+which curl >/dev/null || PREPKG="$PREPKG curl"
+which java >/dev/null || PREPKG="$PREPKG default-jdk"
+[ -n "$PREPKG" ] && sudo apt-get install -y $PREPKG
+
+# do it
 cd ~/Downloads
 curl -s -O -C - http://eclipse.stu.edu.tw/technology/epp/downloads/release/kepler/SR1/eclipse-cpp-kepler-SR1-linux-gtk-x86_64.tar.gz
 
+mkdir -p ~/opt
 cd ~/opt
 if [ -d eclipse ]; then
 	echo `pwd`/eclipse/ existed\!
