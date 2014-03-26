359b787195db649d9a33b6f05fe51b9042b7f5d9
song10 <song10@gmail.com>
2014-03-26 12:42:37 +0800

    refine

--- install-git.sh
+++ install-git.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+which add-apt-repository >/dev/null || sudo apt-get install -y python-software-properties
 sudo add-apt-repository ppa:git-core/ppa
 sudo apt-get update
 sudo apt-get -y install git
--- install-node.sh
+++ install-node.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 which curl >/dev/null || sudo apt-get install -y curl
 curl -s https://raw.github.com/creationix/nvm/master/install.sh | sh
-[[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh # This loads NVM
+[ -s $HOME/.nvm/nvm.sh ] && . $HOME/.nvm/nvm.sh # This loads NVM
 NV=0.11.11
 nvm install $NV
 nvm alias default $NV
