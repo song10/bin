61b380a9affabe53a2b1bb2534a46dcb319a97e5
song10 <song10@gmail.com>
2014-05-22 01:52:25 +0000

    +  install-node.sh

--- install-node.sh
+++ install-node.sh
@@ -4,7 +4,7 @@
 wget -qO- https://raw.github.com/creationix/nvm/master/install.sh | sh
 [ -s $HOME/.nvm/nvm.sh ] && . $HOME/.nvm/nvm.sh # This loads NVM
 #NV=0.11.12
-NV=0.10.26
+NV=0.10.28
 nvm install $NV
 nvm alias default $NV
 nvm use default
