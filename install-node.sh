451619dcd9d86f72641bcd2ee9968278e39a60aa
song10 <song10@gmail.com>
2014-06-07 17:44:10 +0800

    + install-node.sh

--- install-node.sh
+++ install-node.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-#which curl >/dev/null || sudo apt-get install -y curl
+## nvm needs curl
+which curl >/dev/null || sudo apt-get install -y curl
+
 #curl -s https://raw.github.com/creationix/nvm/master/install.sh | sh
 wget -qO- https://raw.github.com/creationix/nvm/master/install.sh | sh
 [ -s $HOME/.nvm/nvm.sh ] && . $HOME/.nvm/nvm.sh # This loads NVM
+
 #NV=0.11.12
 NV=0.10.28
 nvm install $NV
