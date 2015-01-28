4b1c4d8ea3031ba2b3ae1d3ab9fe3591d666486f
song10 <song10@gmail.com>
2015-01-28 23:41:43 +0800

    + install-mongoose.sh

--- install-mongoose.sh
+++ install-mongoose.sh
@@ -7,14 +7,22 @@ else
 fi
 
 cd mongoose
-if [ ! -d Build ]; then
+
+if [ -d examples/web_server ]; then
+	pushd examples/web_server >/dev/null
+	make
+	mkdir -p ~/opt/bin
+	ln -s $(readlink -f ./web_server) ~/opt/bin/mongoose
+	popd >/dev/null
+elif [ ! -d Build ]; then
 	git co 5.1
 	mv build Build
 	git co master
-fi
 
-cd Build
-sed -i -e 's/mg_create_server(NULL)/mg_create_server(NULL,NULL)/' main.c
-make
-mkdir -p ~/my/opt/bin
-cp mongoose ~/my/opt/bin
+	pushd Build >/dev/null
+	sed -i -e 's/mg_create_server(NULL)/mg_create_server(NULL,NULL)/' main.c
+	make
+	mkdir -p ~/opt/bin
+	ln -s $(readlink -f ./mongoose) ~/opt/bin/mongoose
+	popd >/dev/null
+fi
