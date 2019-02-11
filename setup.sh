67d32b42de19f86292ed8fd1d1cef96d28351071
Rex Zhuo <rex.zhuo@outlook.com>
2019-02-11 16:44:50 +0800

    add rc/.bash_my_bin

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-FILES=".bashrc_alias .bashrc_env .bashrc_path .bashrc_prompt .bashrc_func"
+XPATH=$(readlink -f $(dirname "$0"))
+FILES=".bash_my_bin .bashrc_alias .bashrc_env .bashrc_path .bashrc_prompt .bashrc_func"
 FILES="$FILES .andes-proxy-env .no-andes-proxy-env"
 FILES="$FILES .gitconfig .screenrc"
 # FILES="$FILES env.v5"
 cd $HOME
 if [ -z "$1" ]; then
   for x in $FILES; do
-    ln -s ~/my/bin/rc/$x
+    ln -s "$XPATH/rc/$x"
   done
 else
   rm -f $FILES
