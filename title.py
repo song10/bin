5f2979fee08287eee6562b025120a4b67adb4762
Rex Zhuo <rex.zhuo@outlook.com>
2019-02-12 11:51:21 +0800

    title.py: add screen support

--- title.py
+++ title.py
@@ -3,17 +3,73 @@ import os
 import sys
 from string import Template
 
-# for GNOME Terminal
-template = r'''
-PROMPT_COMMAND='echo -ne "\033]0;$title\007"'
-'''
+title = None
+type = []
+
+# parse argv
+for arg in sys.argv[1:]:
+    if len(arg) > 0 and arg[0] == '-':
+        type.append(arg)
+    else:
+        title = arg
+
+# default title
+if title == None:
+    title = os.uname()[1]
+# print('a "%s"' % title)
+
+# default type
+if len(type) == 0:
+    term = os.environ.get('TERM', '')
+    sty = os.environ.get('STY', '')
+    typ = '-t' if term != 'screen' and sty == '' else '-s'
+    type.append(typ)
+# print(type)
+
+# normalize title
+if len(title):
+    path = os.getcwd()
+    if title == '.':
+        title = os.path.basename(path)
+        if title == '':
+            title = os.sep
+    elif title == '..':
+        title = os.path.relpath(path, '%s/../..' % path)
+        if title == '.':
+            title = os.sep
+        elif not os.sep in title:
+            title = os.sep + title
+# print(title)
+
+# normalize type
+std_type = []
+for typ in type:
+    for x in typ:
+        if x == '-': continue
+        std_type.append(x)
+# print(std_type)
 
 # generate shell script
-title = sys.argv[1] if len(sys.argv) > 1 else os.path.basename(os.getcwd())
-d = dict(title=title)
-t = Template(template)
-script = t.substitute(d)[1:-1]
-# print('script', script)
+script = ':'
+for typ in std_type:
+    if typ == 't':
+        # for GNOME Terminal
+        template = r'''
+;PROMPT_COMMAND='echo -ne "\033]0;$title\007"'
+'''
+        d = dict(title=title)
+        t = Template(template)
+        script += t.substitute(d)[1:-1]
+    elif typ == 's':
+        # for GNU screen
+        template = r'''
+;PROMPT_COMMAND='echo -ne "\033k$title\033\\"'
+'''
+        d = dict(title=title)
+        t = Template(template)
+        script += t.substitute(d)[1:-1]
+    else:
+        script += ';echo "type \'%s\' not supported!"' % typ
 
 # done
 print(script)
