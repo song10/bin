ebb96dbaed3e009b88c8629722c2b23a4826ed8e
Rex Zhuo <rex.zhuo@outlook.com>
2019-02-14 09:59:04 +0800

    title.py: add tmux support

--- title.py
+++ title.py
@@ -3,13 +3,18 @@ import os
 import sys
 from string import Template
 
+# term_type
+# -x for xterm
+# -s for screen
+# -t for tmux
+#
 title = None
-type = []
+term_type = []
 
 # parse argv
 for arg in sys.argv[1:]:
     if len(arg) > 0 and arg[0] == '-':
-        type.append(arg)
+        term_type.append(arg)
     else:
         title = arg
 
@@ -18,13 +23,14 @@ if title == None:
     title = os.uname()[1]
 # print('a "%s"' % title)
 
-# default type
-if len(type) == 0:
-    term = os.environ.get('TERM', '')
-    sty = os.environ.get('STY', '')
-    typ = '-t' if term != 'screen' and sty == '' else '-s'
-    type.append(typ)
-# print(type)
+# default term_type
+if len(term_type) == 0:
+    # term = os.environ.get('TERM')
+    sty = os.environ.get('STY')
+    tmux = os.environ.get('TMUX')
+    typ = '-s' if sty else '-t' if tmux else '-x'
+    term_type.append(typ)
+# print(term_type)
 
 # normalize title
 if len(title):
@@ -41,35 +47,37 @@ if len(title):
             title = os.sep + title
 # print(title)
 
-# normalize type
+# normalize term_type
 std_type = []
-for typ in type:
+for typ in term_type:
     for x in typ:
         if x == '-': continue
         std_type.append(x)
 # print(std_type)
 
 # generate shell script
-script = ':'
+script = ''
 for typ in std_type:
-    if typ == 't':
-        # for GNOME Terminal
-        template = r'''
-;PROMPT_COMMAND='echo -ne "\033]0;$title\007"'
-'''
+    if typ == 'x':
+        # for xterm
+        template = r'''PROMPT_COMMAND='echo -ne "\033]0;$title\007"';'''
         d = dict(title=title)
         t = Template(template)
-        script += t.substitute(d)[1:-1]
+        script += t.substitute(d)
     elif typ == 's':
-        # for GNU screen
-        template = r'''
-;PROMPT_COMMAND='echo -ne "\033k$title\033\\"'
-'''
+        # for screen
+        template = r'''PROMPT_COMMAND='echo -ne "\033k$title\033\\"';'''
         d = dict(title=title)
         t = Template(template)
-        script += t.substitute(d)[1:-1]
+        script += t.substitute(d)
+    elif typ == 't':
+        # for tmux
+        template = r'''tmux rename-window '$title';'''
+        d = dict(title=title)
+        t = Template(template)
+        script += t.substitute(d)
     else:
-        script += ';echo "type \'%s\' not supported!"' % typ
+        script += 'echo "term_type \'%s\' not supported!";' % typ
 
 # done
 print(script)
